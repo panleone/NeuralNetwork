54c8c9255f7085718d3d71f06a8599408d175ccd
panleone <alessandrorezzi2000@gmail.com>
2026-03-03 11:56:29 +0100

    refactor: split tests in their own executable

--- src/tests/test_runner.cpp
+++ src/tests/test_runner.cpp
@@ -2,10 +2,19 @@
 #include "convolution_tests_2d.h"
 #include "nn_tests.h"
 #include "shared_node_tests.h"
+#include <stdexcept>
+#include <iostream>
 
-void run_tests() {
-    shared_node_tests();
-    convolution_tests_1d();
-    convolution_tests_2d();
-    nn_tests();
+int main() {
+    try{
+        shared_node_tests();
+        convolution_tests_1d();
+        convolution_tests_2d();
+        nn_tests();
+    } catch (const std::runtime_error& e){
+        std::cerr << "Unit tests failed: " << e.what() << std::endl;
+        return 1;
+    }
+    std::cout << "All tests passed" << std::endl;
+    return 0;
 }
