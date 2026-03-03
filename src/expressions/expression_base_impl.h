1a41786c41b80ab897b788a029533ccd21fd5627
Alessandro Rezzi <a.rezzi@dwf-dt.com>
2026-03-03 10:50:45 +0100

    feat: use the new VisitorManager in the expression system

--- src/expressions/expression_base_impl.h
+++ src/expressions/expression_base_impl.h
@@ -22,18 +22,19 @@
 template <typename Expr>
 auto DExpr<Expr>::get_parameters() const {
     using T = IntrinsicType::Type;
-    GetParametersVisitor<T> visitor{};
-
-    static_cast<const Expr &>(*this).traverse(visitor);
-    return visitor.res;
+    return visitor_manager.visit<GetParametersVisitor<T>>(static_cast<const Expr &>(*this));
 }
 
 template <typename Expr>
 auto DExpr<Expr>::collect_tensor_handles() const {
     using T = IntrinsicType::Type;
     constexpr size_t num_tensors = Expr::template traverse<GetNumTensorHandlesVisitor<T>>();
-    GetTensorHandlesVisitor<T, num_tensors> visitor{};
 
-    static_cast<const Expr &>(*this).traverse(visitor);
-    return visitor.res;
+    return visitor_manager.visit<GetTensorHandlesVisitor<T, num_tensors>>(
+        static_cast<const Expr &>(*this));
+}
+
+template <typename Expr>
+void DExpr<Expr>::post_backprop_cleanup() {
+    visitor_manager.visit<ResetSharedNodesVisitor>(static_cast<Expr &>(*this));
 }
